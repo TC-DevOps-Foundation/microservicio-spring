390ad2a3c76333a2135a4a6283f90e816157f738
Tomas Castro <tomas.castro.gonzalez@hotmail.com>
2023-05-21 00:14:00 -0400

    Se modifica mensaje

--- src/main/java/com/example/testingweb/HomeController.java
+++ src/main/java/com/example/testingweb/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
 	@RequestMapping("/")
 	public @ResponseBody String greeting() {
-		return "Finaliza el curso DevOps Foundation V4 - Tomas Castro";
+		return "Hello, World";
 	}
 
 }
